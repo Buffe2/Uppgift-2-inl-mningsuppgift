fc4e32a924d8e2518c30c728e188f5563c2ab3bb
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2023-02-01 10:12:09 +0100

    Add project files.

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Uppgift_2._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin i meter?");
+            string strNr1 = Console.ReadLine();
+            double fltNr1 = Convert.ToDouble(strNr1);
+            Console.WriteLine("Hur långt hoppade Alma i meter?");
+            string strNr2 = Console.ReadLine();
+            double fltNr2 = Convert.ToDouble(strNr2);
+            double skillnad = fltNr1 - fltNr2;
+            Console.WriteLine("Elin hoppade " + skillnad + " meter längre än Alma.");
+        }
+    }
+}
+
